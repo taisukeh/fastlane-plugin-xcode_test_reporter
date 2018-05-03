76d294c648d718c523d89391339586f9088b3ea1
Taisuke Hori <taisuke.horix@gmail.com>
2018-05-04 03:27:56 +0900

    Fix

--- spec/xcode_test_reporter_action_spec.rb
+++ spec/xcode_test_reporter_action_spec.rb
@@ -1,9 +1,9 @@
 describe Fastlane::Actions::XcodeTestReporterAction do
   describe '#run' do
     it 'prints a message' do
-      expect(Fastlane::UI).to receive(:user_error!).with("xcode-test-reporter command failed.")
-
-      Fastlane::Actions::XcodeTestReporterAction.run({})
+      expect {
+        Fastlane::Actions::XcodeTestReporterAction.run({})
+      }.to raise_error(FastlaneCore::Interface::FastlaneError)
     end
   end
 end
